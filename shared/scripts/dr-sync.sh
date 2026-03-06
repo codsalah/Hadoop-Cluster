9abb6ebc18941f0f3f2af11310ce5125e85418d8
AhmedWaheedM <ahmedwaheed2003@gmail.com>
2026-03-06 19:33:24 +0200

    refactor: refactored dr-sync.sh file to dynamically locate the active NN and start the syncing process correctly

--- shared/scripts/dr-sync.sh
+++ shared/scripts/dr-sync.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
-# dr-sync.sh — Disaster Recovery Pull Synchronization
-# Executes DistCp and Rsync to mirror the Primary Cluster to this DR Cluster.
+echo "Starting Disaster Recovery Synchronization..."
 
-set -e
+PRIMARY_TAILSCALE_IP="100.75.183.48"
 
-HADOOP_BIN="/opt/hadoop/bin/hadoop"
-PRIMARY_TAILSCALE_IP="100.75.183.48" # Primary Site VPN IP
-PRIMARY_NN="hdfs://node01:8020"
-LOCAL_NN="hdfs://dr-node01:8020"
+# 1. Hunt for the Active Primary (Source) NameNode
+if hdfs dfs -ls hdfs://node01:8020/ > /dev/null 2>&1; then
+    PRIMARY_NN="node01"
+elif hdfs dfs -ls hdfs://node02:8020/ > /dev/null 2>&1; then
+    PRIMARY_NN="node02"
+else
+    echo "CRITICAL ERROR: No Active NameNode found on the Primary Cluster!"
+    exit 1
+fi
 
-echo "[$(date '+%H:%M:%S')] Starting Disaster Recovery Synchronization..."
-# 1. DistCp (HDFS Data Synchronization)
-echo "[$(date '+%H:%M:%S')] Initiating DistCp MapReduce job from Primary..."
-$HADOOP_BIN distcp "-Ddfs.client.use.datanode.hostname=true" -update -pt $PRIMARY_NN/ $LOCAL_NN/
-echo "[$(date '+%H:%M:%S')] DistCp HDFS synchronization complete."
-echo "[$(date '+%H:%M:%S')] Waiting for 3 seconds before Rsync..."
-sleep 3
+# 2. Hunt for the Active DR (Target) NameNode
+if hdfs dfs -ls hdfs://dr-node01:8020/ > /dev/null 2>&1; then
+    DR_NN="dr-node01"
+elif hdfs dfs -ls hdfs://dr-node02:8020/ > /dev/null 2>&1; then
+    DR_NN="dr-node02"
+else
+    echo "CRITICAL ERROR: No Active NameNode found on the DR Vault!"
+    exit 1
+fi
 
-# 2. Rsync (OS-Level Metadata Backup)
+echo "Active Primary found at: $PRIMARY_NN"
+echo "Active DR Vault found at: $DR_NN"
+echo "Initiating DistCp MapReduce job..."
+
+# 3. Execute DistCp dynamically
+/opt/hadoop/bin/hadoop distcp -Ddfs.client.use.datanode.hostname=true -update -pt hdfs://${PRIMARY_NN}:8020/ hdfs://${DR_NN}:8020/
+
+# 4. Rsync (OS-Level Metadata Backup)
 echo "[$(date '+%H:%M:%S')] Initiating Rsync for NameNode Metadata..."
 mkdir -p /shared/dr-backups/namenode/
-rsync -avz  -e "ssh -i /shared/dr_rsa_key -o StrictHostKeyChecking=no" --rsync-path="docker exec -i node01 rsync" root@$PRIMARY_TAILSCALE_IP:/var/hadoop/namenode/ /shared/dr-backups/namenode/
+
+# Your working Rsync command, now 100% dynamic
+rsync -avz -e "ssh -i /shared/dr_rsa_key -o StrictHostKeyChecking=no" --rsync-path="docker exec -i $PRIMARY_NN rsync" root@${PRIMARY_TAILSCALE_IP}:/var/hadoop/namenode/ /shared/dr-backups/namenode/
+
+echo "Disaster Recovery Synchronization Complete."
